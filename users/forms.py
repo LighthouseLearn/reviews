00bac9b800270764372906d27d377f942ec21e91
Jin <jinzhang455@gmail.com>
2019-11-28 17:45:07 -0800

    worked on getting new user field to show in sign up

--- users/forms.py
+++ users/forms.py
@@ -3,11 +3,18 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
+    DESIGNATION = (
+        ('Undergrad', 'Undergrad'),
+        ('Grad Student', 'Grad Student'),
+        ('Alumni', 'Alumni'),
+        ('Not a student', 'Not a student'),
+    )
     email = forms.CharField(max_length=254, required=True, widget=forms.EmailInput())
-
+    student_designation = forms.ChoiceField(widget=forms.Select, choices=DESIGNATION)
+    
     class Meta(UserCreationForm.Meta):
         model = CustomUser
-        fields = ('username', 'email', 'student_designation' )
+        fields = ('username', 'email', 'student_designation')
 
 
 class CustomUserChangeForm(UserChangeForm):
