76da302c9d9b036612a56bc5f3eaab6e1365b066
Jin <jinzhang455@gmail.com>
2020-02-10 15:50:03 -0800

    added pages, changed colors, form validate for signup and login as well as bootstrap changes, added home background added templatetags, changed form

--- pages/urls.py
+++ pages/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
+    path('tos/', views.TosView.as_view(), name='tos'),
+    path('about/', views.AboutView.as_view(), name='about'),
+    path('faq/', views.FaqView.as_view(), name='faq'),
 ]
 
--- pages/views.py
+++ pages/views.py
@@ -4,3 +4,12 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
+class TosView(TemplateView):
+    template_name = 'tos.html'
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+class FaqView(TemplateView):
+    template_name = 'faq.html'
