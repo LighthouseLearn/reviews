ce8554355905214eddd68977fec18158ae07604a
Jin <jinzhang455@gmail.com>
2020-02-10 16:52:51 -0800

    changes to url path in pages

--- pages/urls.py
+++ pages/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
-    path('tos/', views.TosPageView.as_view(), name='tos'),
-    path('about/', views.AboutPageView.as_view(), name='about'),
-    path('faq/', views.FaqPageView.as_view(), name='faq'),
+    path('tos', views.TosPageView.as_view(), name='tos'),
+    path('about', views.AboutPageView.as_view(), name='about'),
+    path('faq', views.FaqPageView.as_view(), name='faq'),
 ]
 
